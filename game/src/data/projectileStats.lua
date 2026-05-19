60613e93f003c74c22a23fd36216b1bb8e8054e5
Marius Heinrichs <mariusheinrichs@gmail.com>
2026-05-19 22:27:01 +0200

    Update projectile and unit stats; adjust velocities, damage, attack ranges, and movement speeds for balance

--- game/src/data/projectileStats.lua
+++ game/src/data/projectileStats.lua
@@ -1,14 +1,14 @@
 -- Projectile-Stats
 return {
   Arrow = {
-    Velocity = 8.0,
-    Damage = 12,
+    Velocity = 25.0,
+    Damage = 20,
     SplashRadius = 0,
     SplashDamageMultiplier = 0,
   },
   Fireball = {
-    Velocity = 6.0,
-    Damage = 22,
+    Velocity = 20.0,
+    Damage = 30,
     SplashRadius = 6.0,
     SplashDamageMultiplier = 0.3,
   }
